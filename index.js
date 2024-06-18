9c5554b1c388f10429cd88e6b488d60975f6dfa7
崮生 <admin@shenzilong.cn>
2024-06-18 13:50:46 +0000

    Update dist to GitHub Pages

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
 "use strict";
-var _documentCurrentScript = typeof document !== "undefined" ? document.currentScript : null;
-console.log(222, typeof document === "undefined" ? require("url").pathToFileURL(__filename).href : _documentCurrentScript && _documentCurrentScript.src || new URL("index.js", document.baseURI).href);
+var __defProp = Object.defineProperty;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+const siyuan = require("siyuan");
+const rawRequire = globalThis.require;
+globalThis.require = (moduleName) => {
+  if (moduleName === "siyuan") {
+    return siyuan;
+  }
+  return rawRequire(moduleName);
+};
+class VitePlugin extends siyuan.Plugin {
+  constructor() {
+    super(...arguments);
+    __publicField(this, "layoutRead", false);
+  }
+  async onload() {
+    globalThis.vitePlugin = this;
+    const url = await this.loadData("url");
+    if (!url) return;
+    this.loadByUrl(url);
+  }
+  onLayoutReady() {
+    this.layoutRead = true;
+  }
+  loadByUrl(url) {
+    this.saveData("url", url);
+    let src = `${url}?t=${Date.now()}`;
+    import(src).then((module2) => {
+      const pluginClass = module2.default;
+      const plugin = new pluginClass({
+        app: this.app,
+        displayName: pluginClass.name,
+        name: pluginClass.name,
+        i18n: {}
+      });
+      this.app.plugins.push(plugin);
+      plugin.onload();
+      if (this.layoutRead) {
+        plugin.onLayoutReady();
+      }
+      console.log("[load plugin]", { module: module2, pluginClass, plugin });
+    });
+  }
+  async setViteUrl(url) {
+    await this.saveData("url", url);
+    this.loadByUrl(url);
+  }
+}
+module.exports = VitePlugin;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,6 +1,51 @@
 // 引入这个变量后 vite 会自动注入 hot
-import.meta.hot
-import "./icon.png"
-console.log(222,import.meta.url);
+import.meta.hot;
+import siyuan from "siyuan";
 
-export const run = 222
+//@ts-ignore
+const rawRequire = globalThis.require;
+//@ts-ignore
+globalThis.require = (moduleName: string) => {
+  if (moduleName === "siyuan") {
+    return siyuan;
+  }
+  return rawRequire(moduleName);
+};
+
+export default class VitePlugin extends siyuan.Plugin {
+  async onload() {
+    // @ts-ignore
+    globalThis.vitePlugin = this;
+    const url = await this.loadData("url");
+    if (!url) return;
+    this.loadByUrl(url);
+  }
+  layoutRead = false;
+  onLayoutReady(): void {
+    this.layoutRead = true;
+  }
+  public loadByUrl(url: string) {
+    this.saveData("url", url);
+
+    let src = `${url}?t=${Date.now()}`;
+    import(src).then((module) => {
+      const pluginClass = module.default;
+      const plugin = new pluginClass({
+        app: this.app,
+        displayName: pluginClass.name,
+        name: pluginClass.name,
+        i18n: {},
+      }) as siyuan.Plugin;
+      this.app.plugins.push(plugin);
+      plugin.onload();
+      if (this.layoutRead) {
+        plugin.onLayoutReady();
+      }
+      console.log("[load plugin]", { module, pluginClass, plugin });
+    });
+  }
+  public async setViteUrl(url: string) {
+    await this.saveData("url", url);
+    this.loadByUrl(url);
+  }
+}
