2a26a9ef38a8237c1a7521f6a6317a78b1f4c7bf
崮生 <admin@shenzilong.cn>
2024-06-18 10:07:13 +0000

    Update dist to GitHub Pages

---
+++ vite-plugin-siyuan/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var _documentCurrentScript = typeof document !== "undefined" ? document.currentScript : null;
+console.log(222, typeof document === "undefined" ? require("url").pathToFileURL(__filename).href : _documentCurrentScript && _documentCurrentScript.src || new URL("index.js", document.baseURI).href);
+//# sourceMappingURL=index.js.map
